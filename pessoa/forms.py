652674f206517ea44b5c201f258ed544ba8e2ed5
Marcelo Avanzi <marcelo.d.avanzi@exxonmobil.com>
2022-07-19 20:30:44 -0300

    Implementando formularios

--- pessoa/forms.py
+++ pessoa/forms.py
@@ -1,8 +1,14 @@
-from django.forms import fields, models, ModelForm
+import re
+from django.forms import fields, models, ModelForm, DateField, TextInput
 from .models import Pessoa
 
 
 class PessoaForm(ModelForm):
+    data_nascimento = DateField(
+        widget=TextInput(
+            attrs = {"type": "date"}
+        )
+    , required=True)
     class Meta:
         model = Pessoa
         fields = ['nome_completo', 'data_nascimento', 'ativa']
